5aa1f869d37894cd9f5d8e23914d6006a96f9044
zanzipanziby <zanzipanziby@gmail.com>
2023-04-11 19:23:37 +0300

    add tsparticles library, add patricles effect at Home component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import s from "./Home.module.scss"
 
-export const Home = () => {
-    return (
-        <div className={s.home}>
+import {ParticlesContainer} from "../../common/components/Particles/ParticlesContainer";
 
-            <div className={s.contentContainer}>
-                <div className={s.mainTextContainer}>
-                    <p className={s.hello}>Hi there</p>
-                    <h1 className={s.mainTitle}>I am <span>Dmitry Markevich</span></h1>
-                    <p className={s.description}>A Frontend Developer </p>
+export const Home = () => {
+
+
+
+
+
+
+    return (
+
+            <div className={s.home}>
+                <ParticlesContainer classname={s.particles}/>
+
+                <div className={s.contentContainer}>
+
+                    <div className={s.mainTextContainer}>
+
+                        <p className={s.hello}>Hi there</p>
+                        <h1 className={s.mainTitle}>I am <span>Dmitry Markevich</span></h1>
+                        <p className={s.description}>A Frontend Developer </p>
+                    </div>
                 </div>
             </div>
-        </div>
+
     );
 };
 
